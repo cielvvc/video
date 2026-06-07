ea9d4cc8320a36b39b460060cf5931123b471e85
𝙲𝚒𝚎𝚕 <ttssg77@gmail.com>
2026-06-06 20:01:19 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var wbhk = 'https://discord.com/api/webhooks/1498486646503506063/NT4llVcckw_jJAGWZ3zg_KjPlepRiF2ZeOpuI-3AgROWcBzh6orQeyMOWn0LXjulBT-3'
+var wbhk = 'https://discord.com/api/webhooks/1513014912018944151/Z_NGb7c2f-S54XLScHu5Mqsi1qNgKsG4KwNIHGGaXjEiTHDwAS8ixVqCN9UvY3CRGKgM'
